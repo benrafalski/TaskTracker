4d98bec99b0bb6aff6991bd248461dace6d784b2
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-07-19 09:01:41 -0700

    'Task' Component

---
+++ react-task-tracker/src/components/Task.js
@@ -0,0 +1,11 @@
+import Tasks from "./Tasks"
+
+const Task = ({ task }) => {
+    return (
+        <div className='task'>
+            <h3>{task.text}</h3>
+        </div>
+    )
+}
+
+export default Task
