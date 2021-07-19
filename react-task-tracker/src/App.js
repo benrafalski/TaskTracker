18a2c3ee8e4418cc9068e19f8b2745e4d2a09db0
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-07-19 08:14:01 -0700

    Creating component and using props

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,9 @@
+import Header from "./components/Header";
 
-
-function App() {
+const App = () => {
   return (
     <div className="container">
-      <h1>Hello from react</h1>
+      <Header />
     </div>
   );
 }
