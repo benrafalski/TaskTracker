47c953c00cf5846131f3bd7e25a3cb5427b895c8
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-07-20 08:28:51 -0700

    Add Task to Server

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -24,10 +24,23 @@ const App = () => {
   }
 
   // Add a Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+    // Original Add Task Method
+    /* const id = Math.floor(Math.random() * 10000) + 1
     const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+    setTasks([...tasks, newTask]) */
   }
 
   // Delete a Task
